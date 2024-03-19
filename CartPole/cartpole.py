59ae51aa3511c0cd8cae242525d2381e277f1f3c
Hanwen Ren <ren221@purdue.edu>
2024-03-19 17:16:21 -0400

    code clean

--- CartPole/cartpole.py
+++ CartPole/cartpole.py
@@ -173,17 +173,3 @@ if __name__ == '__main__':
 		else:
 			evaluate(random = False)
 
-sys.exit(1)
-
-
-env = gym.make("CartPole-v1", render_mode='human')
-env.reset()
-#observation, info = env.reset(seed=42)
-for _ in range(5000):
-	env.render()
-	action = env.action_space.sample()
-	observation, reward, terminated, truncated, info = env.step(action)
-
-	if terminated or truncated:
-		observation, info = env.reset()
-env.close()
